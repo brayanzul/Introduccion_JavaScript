2114965a9ce7b4fef4c5c46e62a21fbe30d333e4
Brayan Zuluaga <brayanzuluaga93@hotmail.com>
2022-12-18 10:31:07 -0500

    Add Introduction JavaScript

---
+++ public_html/funciones.js
@@ -0,0 +1,12 @@
+/** 
+ * @author brayan
+*/ 
+function saluda(){
+    alert("Hola Mundo");
+}
+
+function escribeMensaje(){
+    document.getElementById("mensajeHtml").innerHTML = "Saludo desde archivode JavaScript";
+}
+
+
